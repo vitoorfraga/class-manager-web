fce6e3ec9bf76db22ab57e12c908bca7959a7f20
Vitor Fraga <vitorfragaps@gmail.com>
2024-08-04 22:13:48 -0300

    feat: add SignIn page

---
+++ src/app/(public)/sign-in/_components/sign-in-form.tsx
@@ -0,0 +1,56 @@
+import Link from "next/link"
+
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+export const SignInForm = () => {
+  return (
+    <Card className="mx-auto max-w-sm">
+      <CardHeader>
+        <CardTitle className="text-2xl">Entrar</CardTitle>
+        <CardDescription>
+          Insira seu e-mail abaixo para fazer login em sua conta
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div className="grid gap-4">
+          <div className="grid gap-2">
+            <Label htmlFor="email">E-mail</Label>
+            <Input
+              id="email"
+              type="email"
+              placeholder="m@exemplo.com"
+              required
+            />
+          </div>
+          <div className="grid gap-2">
+            <div className="flex items-center">
+              <Label htmlFor="password">Senha</Label>
+              <Link href="#" className="ml-auto inline-block text-sm underline">
+                Esqueceu sua senha?
+              </Link>
+            </div>
+            <Input id="password" type="password" required />
+          </div>
+          <Button type="submit" className="w-full">
+            Entrar
+          </Button>
+        </div>
+        <div className="mt-4 text-center text-sm">
+           Não tem uma conta? Inscreva-se
+          <Link href="#" className="underline">
+            Inscrever-se
+          </Link>
+        </div>
+      </CardContent>
+    </Card>
+  )
+}
---
+++ src/app/(public)/sign-in/page.tsx
@@ -0,0 +1,9 @@
+import { SignInForm } from "./_components/sign-in-form";
+
+export default function SignInPage() {
+  return (
+    <main className="h-screen flex justify-center items-center">
+      <SignInForm />
+    </main>
+  )
+}
